4fa71f3dfb2e23b95522d3e5cc0494f54cc9c11f
AlexParker82 <noyes.parker@gmail.com>
2023-02-22 07:47:58 -0500

    more navbar and button styling

--- src/chakra/theme.ts
+++ src/chakra/theme.ts
@@ -1,8 +1,9 @@
 // 1. Import `extendTheme`
-import { extendTheme } from "@chakra-ui/react"
-import "@fontsource/open-sans/300.css"
-import "@fontsource/open-sans/400.css"
-import "@fontsource/open-sans/700.css"
+import { extendTheme } from "@chakra-ui/react";
+import "@fontsource/open-sans/300.css";
+import "@fontsource/open-sans/400.css";
+import "@fontsource/open-sans/700.css";
+import { Button } from "./button";
 
 // 2. Call `extendTheme` and pass your custom values
 export const theme = extendTheme({
@@ -16,12 +17,12 @@ export const theme = extendTheme({
   },
   styles: {
     global: () => ({
-        body: {
-            bg: "gray.200"
-        },
+      body: {
+        bg: "gray.200",
+      },
     }),
   },
   components: {
-    // Button
-  }
-})
+    Button,
+  },
+});
--- src/components/Navbar/SearchInput.tsx
+++ src/components/Navbar/SearchInput.tsx
@@ -9,12 +9,29 @@ type SearchInputProps = {
 const SearchInput: React.FC<SearchInputProps> = () => {
 
     return (
-        <Flex align="center">
+        <Flex align="center" flexGrow={1} mr={2}>
             <InputGroup>
                 <InputLeftElement pointerEvents='none'>
-                    <SearchIcon color='gray.300' />
+                    <SearchIcon color='gray.500' mb={1} />
                 </InputLeftElement>
-                <Input type='search' placeholder='Search Reddit' />
+                <Input
+                    type='search'
+                    placeholder='Search Reddit'
+                    fontSize="10pt"
+                    _placeholder={{ color: "gray.500" }}
+                    _hover={{
+                        bg: "white",
+                        border: "1px solid",
+                        borderColor: "blue.500"
+                    }}
+                    _focus={{
+                        outline: "none",
+                        border: "1px solid",
+                        borderColor: "blue.500"
+                    }}
+                    height="34px"
+                    bg="gray.50"
+                />
             </InputGroup>
         </Flex>
     )
