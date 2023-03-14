474e30cc05c7d14b01831def3dcba024f76ae40f
AlexParker82 <noyes.parker@gmail.com>
2023-03-14 13:15:49 -0400

    working on communities functionality and pages

--- src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
+++ src/components/Modal/CreateCommunity/CreateCommunityModal.tsx
@@ -1,5 +1,10 @@
-import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Box, Text, Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { auth, firestore } from '@/firebase/clientApp';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, Box, Text, Input, InputGroup, InputLeftElement, Flex, Radio, RadioGroup, Stack, Icon } from '@chakra-ui/react';
+import { doc, getDoc, runTransaction, serverTimestamp, setDoc } from 'firebase/firestore';
 import React, { useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { BsFillEyeFill, BsFillPersonFill } from 'react-icons/bs';
+import { HiLockClosed } from 'react-icons/hi';
 
 type CreateCommunityModalProps = {
   open: boolean;
@@ -9,8 +14,12 @@ type CreateCommunityModalProps = {
 const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handleClose }) => {
   const [community, setCommunity] = useState("");
   const [characters, setCharacters] = useState(21);
+  const [radioValue, setRadioValue] = useState("Public");
+  const [error, setError] = useState("");
+  const [user] = useAuthState(auth);
+  const [loading, setLoading] = useState(false);
 
-  const handleCommunity = (e: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputValue = e.target.value;
 
     if (inputValue.length > 21) return;
@@ -19,36 +28,114 @@ const CreateCommunityModal: React.FC<CreateCommunityModalProps> = ({ open, handl
     setCharacters(21 - inputValue.length)
   }
 
+  const handleCommunitySubmit = async () => {
+    setError("");
+    const format = /[`!@#$%^&*()+\-=\[\]{};':"\\|,.<>\/?~]/;
+    const test = format.test(community);
+    if (test || community.length < 3) {
+      setError("Name must be at least 3 characters and can only contain letters, numbers and underscores");
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const communityDocRef = doc(firestore, "communities", community);
+
+      await runTransaction(firestore, async (transaction) => {
+
+        const communityDoc = await transaction.get(communityDocRef);
+        if (communityDoc.exists()) {
+          throw new Error(`Sorry, r/${community} is taken, please try another`);
+        }
+
+        transaction.set(communityDocRef, {
+          creatorId: user?.uid,
+          createdAt: serverTimestamp(),
+          numberOfMembers: 1,
+          privacyType: radioValue
+        });
+
+        transaction.set(doc(firestore, `users/${user?.uid}/communitySnippets`, community), {
+          communityId: community,
+          isModerator: true,
+
+        })
+
+      });
+
+      setLoading(false);
+
+    } catch (error: any) {
+      console.error("handleCommunitySubmit error", error);
+      setError(error.message)
+    }
+    handleClose();
+
+  }
+
+
   return (
     <>
-      <Modal isOpen={open} onClose={handleClose}>
+      <Modal isOpen={open} onClose={handleClose} size="lg">
         <ModalOverlay />
         <ModalContent>
           <ModalHeader display="flex" flexDirection="column" fontSize={15} padding={3}>Create a Community</ModalHeader>
           <Box pl={3} pr={3}>
             <ModalCloseButton />
             <ModalBody display="flex" flexDirection="column" padding="10px 0px">
-              <Text fontWeight={600} fontSize={15}>Name</Text>
-              <Text fontSize={11} color="gray.500">Community names including capitalization cannot be changed</Text>
-              <InputGroup>
-                <InputLeftElement pointerEvents='none'>
-                  <Text mb={2} color="gray.400">r/</Text>
-                </InputLeftElement>
-                <Input
-                  type='text'
-                  size="sm"
-                  value={community}
-                  onChange={handleCommunity}
-                />
-              </InputGroup>
-              <Text fontSize="9pt" color={characters === 0 ? "red" : "gray.500"}>{characters} Characters remaining</Text>
+              <Flex direction="column" mb={3}>
+                <Text fontWeight={600} fontSize={15}>Name</Text>
+                <Text fontSize={11} color="gray.500">Community names including capitalization cannot be changed</Text>
+                <InputGroup>
+                  <InputLeftElement pointerEvents='none'>
+                    <Text mb={2} color="gray.400">r/</Text>
+                  </InputLeftElement>
+                  <Input
+                    type='text'
+                    size="sm"
+                    value={community}
+                    onChange={handleInputChange}
+                  />
+                </InputGroup>
+                <Text fontSize="9pt" color={characters === 0 ? "red" : "gray.500"}>{characters} Characters remaining</Text>
+                {error && <Text fontSize="9pt" color="red">{error}</Text>}
+              </Flex>
+              <Flex direction="column" mt={4} mb={4}>
+                <Text fontWeight={600} fontSize={15} mb={2}>Community Type</Text>
+                <RadioGroup onChange={setRadioValue} name="" value={radioValue}>
+                  <Stack direction='column'>
+                    <Radio value='Public'>
+                      <Flex align="center">
+                        <Icon as={BsFillPersonFill} color="gray.500" mr={1} />
+                        <Text fontSize="10pt" mr={1}>Public</Text>
+                        <Text fontSize="8pt" color="gray.500">Anyone can view, post, and comment to this community</Text>
+                      </Flex>
+                    </Radio>
+                    <Radio value='Restricted'>
+                      <Flex align="center">
+                        <Icon as={BsFillEyeFill} color="gray.500" mr={1} />
+                        <Text fontSize="10pt" mr={1}>Restricted</Text>
+                        <Text fontSize="8pt" color="gray.500">Anyone can view, but only approved users can post to this community</Text>
+                      </Flex>
+                    </Radio>
+                    <Radio value='Private'>
+                      <Flex align="center">
+                        <Icon as={HiLockClosed} color="gray.500" mr={1} />
+                        <Text fontSize="10pt" mr={1}>Private</Text>
+                        <Text fontSize="8pt" color="gray.500">Only approved users can view and submit to this community</Text>
+                      </Flex>
+                    </Radio>
+                  </Stack>
+                </RadioGroup>
+              </Flex>
             </ModalBody>
           </Box>
-          <ModalFooter>
-            <Button colorScheme='blue' mr={3} onClick={handleClose}>
+          <ModalFooter bg="gray.100" borderRadius="0px 0px 10px 10px">
+            <Button variant="outline" height="30px" mr={3} onClick={handleClose}>
               Cancel
             </Button>
-            <Button variant='ghost'>Create Community</Button>
+            <Button onClick={handleCommunitySubmit} isLoading={loading} height="30px" >Create Community</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
